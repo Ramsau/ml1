b088312a3bf1681a997a8812d8027b5a0246c4ed
Christoph Royer <mail@christophroyer.com>
2023-06-05 11:38:33 +0200

    Task 3.2

--- ML1_HW3/skeleton/task3_2.py
+++ ML1_HW3/skeleton/task3_2.py
@@ -15,23 +15,39 @@ if __name__ == '__main__':
 
   #TODO fit a random forest classifier and check how well it performs on the test set after tuning the parameters,
   # report your results
-  rf = ...
+  rf = RandomForestClassifier()
+  rf.fit(X_train, y_train)
+  score = rf.score(X_test, y_test)
+  print(f"Score of Random Forest: {score}")
 
   #TODO fit a SVC and find suitable parameters, report your results
-  svc = ...
+  svc = SVC()
+  svc.fit(X_train, y_train)
+  score = svc.score(X_test, y_test)
+  print(f"Score of SVC: {score}")
 
   # TODO create a bar plot (https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.barh.html#matplotlib.pyplot.barh)
   # of the `feature_importances_` of the RF classifier.
+  plt.bar([i for i in range(len(rf.feature_importances_))], rf.feature_importances_)
+  plt.ylabel("importance")
+  plt.xlabel("feature number")
+  plt.savefig("../plots/randomtree_feat_importances.png")
+  plt.show()
 
   # TODO create another RF classifier
   # Use recursive feature elimination (https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.RFECV.html#sklearn.feature_selection.RFECV)
   # to automatically choose the best number of parameters
   # set `scoring = 'accuracy'` to look for the feature subset with highest accuracy
   # and fit the RFECV to the training data
-  rf = ...
-  rfecv = ...
+  rf = RandomForestClassifier()
+  rfecv = RFECV(rf, scoring='accuracy')
 
   # TODO use the RFECV to transform the training and test dataset -- it automatically removes the least important
   # feature columns from the datasets. You don't have to change y_train or y_test
   # Fit a SVC classifier on the new dataset. Do you see a difference in performance?
-  svc = ...
+  X_train_pruned = rfecv.fit_transform(X_train, y_train)
+  X_test_pruned = rfecv.transform(X_test)
+  svc = SVC()
+  svc.fit(X_train_pruned, y_train)
+  score = svc.score(X_test_pruned, y_test)
+  print(f"Score of SVC on pruned data: {score}")
